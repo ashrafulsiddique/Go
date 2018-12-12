25dc1fd1f6b2d956c70518d8b7d05f2989eb8f3b
ashrafulsiddique <ashrafulsiddique@users.noreply.github.com>
2018-12-12 18:46:08 +0600

    Update login.go

--- app/controller/login.go
+++ app/controller/login.go
@@ -5,10 +5,10 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/shamimiceewu025/go/app/model"
-	"github.com/shamimiceewu025/go/app/shared/passhash"
-	"github.com/shamimiceewu025/go/app/shared/session"
-	"github.com/shamimiceewu025/go/app/shared/view"
+	"github.com/ashrafulsiddique/Go/app/model"
+	"github.com/ashrafulsiddique/Go/app/shared/passhash"
+	"github.com/ashrafulsiddique/Go/app/shared/session"
+	"github.com/ashrafulsiddique/Go/app/shared/view"
 
 	"github.com/gorilla/sessions"
 	"github.com/josephspurrier/csrfbanana"
