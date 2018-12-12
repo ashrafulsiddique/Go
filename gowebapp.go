7d36acaa7bd92889c2b9063ea04a94af821ab732
unknown <shyonsiddique@gmail.com>
2018-12-12 15:15:28 +0600

    first commit

---
+++ gowebapp.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/shamimiceewu025/go/app/route"
+	"github.com/shamimiceewu025/go/app/shared/database"
+	"github.com/shamimiceewu025/go/app/shared/email"
+	"github.com/shamimiceewu025/go/app/shared/jsonconfig"
+	"github.com/shamimiceewu025/go/app/shared/recaptcha"
+	"github.com/shamimiceewu025/go/app/shared/server"
+	"github.com/shamimiceewu025/go/app/shared/session"
+	"github.com/shamimiceewu025/go/app/shared/view"
+	"github.com/shamimiceewu025/go/app/shared/view/plugin"
+)
+
+// *****************************************************************************
+// Application Logic
+// *****************************************************************************
+
+func init() {
+	// Verbose logging with file name and line number
+	log.SetFlags(log.Lshortfile)
+
+	// Use all CPU cores
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+	// Load the configuration file
+	jsonconfig.Load("config"+string(os.PathSeparator)+"config.json", config)
+
+	// Configure the session cookie store
+	session.Configure(config.Session)
+
+	// Connect to database
+	database.Connect(config.Database)
+
+	// Configure the Google reCAPTCHA prior to loading view plugins
+	recaptcha.Configure(config.Recaptcha)
+
+	// Setup the views
+	view.Configure(config.View)
+	view.LoadTemplates(config.Template.Root, config.Template.Children)
+	view.LoadPlugins(
+		plugin.TagHelper(config.View),
+		plugin.NoEscape(),
+		plugin.PrettyTime(),
+		recaptcha.Plugin())
+
+	// Start the listener
+	server.Run(route.LoadHTTP(), route.LoadHTTPS(), config.Server)
+}
+
+// *****************************************************************************
+// Application Settings
+// *****************************************************************************
+
+// config the settings variable
+var config = &configuration{}
+
+// configuration contains the application settings
+type configuration struct {
+	Database  database.Info   `json:"Database"`
+	Email     email.SMTPInfo  `json:"Email"`
+	Recaptcha recaptcha.Info  `json:"Recaptcha"`
+	Server    server.Server   `json:"Server"`
+	Session   session.Session `json:"Session"`
+	Template  view.Template   `json:"Template"`
+	View      view.View       `json:"View"`
+}
+
+// ParseJSON unmarshals bytes to structs
+func (c *configuration) ParseJSON(b []byte) error {
+	return json.Unmarshal(b, &c)
+}
